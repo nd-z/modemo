93987652e6170e8e22a1b07174e597b220411403
Andy Zhang <jz359@cornell.edu>
2017-08-04 11:19:51 -0400

    test now uses the crawler to extract paragraph content; currently tests the first paragraph; crawler isnt perfect bc it splits the periods in titles; should figure out a way to fix that

--- backend/modules/bias_analyzer_test.py
+++ backend/modules/bias_analyzer_test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from tf.bias_analyzer import BiasAnalyzer
+from article_crawler import ArticleCrawler
 import time
 import cPickle
 
@@ -8,7 +9,16 @@ print('if this breaks, remember to add the tf folder to PYTHONPATH and try again
 
 #data = ['Donald Trump is a boorish, stupid man.', 'He is unfit to be President, and too dumb to be in the White House.', 'The Democrats are playing a childish game, and would rather see the current healthcare plan continue to cripple America than to pass a new bill.', 'President Trump embraced a proposal on Wednesday to slash legal immigration to the United States in half within a decade by sharply curtailing the ability of American citizens and legal residents to bring family members into the country.', '']
 
-nyt_data = ['President Trump embraced a proposal on Wednesday to slash legal immigration to the United States in half within a decade by sharply curtailing the ability of American citizens and legal residents to bring family members into the country.', 'The plan would enact the most far reaching changes to the system of legal immigration in decades and represents the president’s latest effort to stem the flow of newcomers to the United States.', 'Since taking office, he has barred many visitors from select Muslim majority countries, limited the influx of refugees, increased immigration arrests and pressed to build a wall along the southern border.', 'In asking Congress to curb legal immigration, Mr. Trump intensified a debate about national identity, economic growth, worker fairness and American values that animated his campaign last year.', 'Critics said the proposal would undercut the fundamental vision of the United States as a haven for the poor and huddled masses, while the president and his allies said the country had taken in too many low-skilled immigrants for too long to the detriment of American workers.', '“This legislation will not only restore our competitive edge in the 21st century, but it will restore the sacred bonds of trust between America and its citizens,” Mr. Trump said at a White House event alongside two Republican senators sponsoring the bill.', '“This legislation demonstrates our compassion for struggling American families who deserve an immigration system that puts their needs first and that puts America first.']
+url = 'https://www.nytimes.com/2017/08/02/us/politics/trump-immigration.html'
+crawler = ArticleCrawler()
+
+paragraphs = crawler.url_content(url)
+
+'''
+nyt_data = ['President Trump embraced a proposal on Wednesday to slash legal immigration to the United States in half within a decade by sharply curtailing the ability of American citizens and legal residents to bring family members into the country.', 'The plan would enact the most far reaching changes to the system of legal immigration in decades and represents the president’s latest effort to stem the flow of newcomers to the United States.', 'Since taking office, he has barred many visitors from select Muslim majority countries, limited the influx of refugees, increased immigration arrests and pressed to build a wall along the southern border.', 'In asking Congress to curb legal immigration, Mr. Trump intensified a debate about national identity, economic growth, worker fairness and American values that animated his campaign last year.', 'Critics said the proposal would undercut the fundamental vision of the United States as a haven for the poor and huddled masses, while the president and his allies said the country had taken in too many low-skilled immigrants for too long to the detriment of American workers.', '“This legislation will not only restore our competitive edge in the 21st century, but it will restore the sacred bonds of trust between America and its citizens,” Mr. Trump said at a White House event alongside two Republican senators sponsoring the bill.', '“This legislation demonstrates our compassion for struggling American families who deserve an immigration system that puts their needs first and that puts America first.']'''
+
+nyt_data = paragraphs[0]
+print(nyt_data)
 
 
 print('initializing bias analyzer')
